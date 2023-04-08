34d0f53c9a0cc85b9eacfbd65d446ab8ff616fb9
Adame <Adame@DESKTOP-TGU2OG3>
2023-04-08 21:47:07 +0200

    Add project files.

---
+++ LinkvertiseBypasser/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace LinkvertiseBypasser
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Enter the Linkvertise URL:");
+            string linkvertiseUrl = Console.ReadLine();
+
+            using var client = new HttpClient();
+            var response = await client.GetAsync($"https://bypass.bot.nu/bypass2?url={linkvertiseUrl}");
+            var responseString = await response.Content.ReadAsStringAsync();
+            var bypassedUrl = JsonSerializer.Deserialize<BypassedUrl>(responseString);
+
+            Console.WriteLine($"Destination: {bypassedUrl.destination}");
+            Console.WriteLine($"Success: {bypassedUrl.success}");
+        }
+    }
+
+    class BypassedUrl
+    {
+        public string destination { get; set; }
+        public bool success { get; set; }
+    }
+}
